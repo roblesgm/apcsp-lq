74cb72c110069d8f0eab636bd43ce41dc1aed3f5
Grayson Robles <grayson04runner@gmail.com>
2019-10-08 10:01:23 -0600

    finished bigger_two

--- list_quiz.rb
+++ list_quiz.rb
@@ -14,3 +14,23 @@ end
 # puts three_even([2, 1, 4, 6]) #false
 # puts three_even([1, 4, 6, 4]) #true
 # puts three_even([]) #false
+
+def bigger_two(list1, list2)
+    set1 = list1[0] + list1[1]
+    set2 = list2[0] + list2[1]
+
+    if set1 > set2
+        return list1
+    end
+
+    if set2 > set1
+        return list2
+    end
+
+    if set1 = set2
+        return list1
+    end
+end
+
+puts bigger_two([1, 2], [3, 4]) #[3, 4]
+puts bigger_two([1, 7], [4, 4]) #[1, 7]
